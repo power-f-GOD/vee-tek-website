ea1cbc7fdbb8bf236ecd4eaeefe8bee708083f13
Sunday Power Inemesit <sundaypower9@gmail.com>
2020-05-16 10:27:05 +0100

    Remove unused vars; modify indentation.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
 
-import {Container, Row} from 'react-bootstrap';
-
 import Landing from './components/Landing';
 import './styles/index.min.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
-    return (
-        <div className='App'>
-            <Landing/>
-        </div>
-
-    );
+  return (
+    <div className='App'>
+      <Landing />
+    </div>
+  );
 };
 
 export default App;
--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import { } from "react-bootstrap";
-import Header from "./Header";
+import {} from 'react-bootstrap';
+import Header from './Header';
 
 const Landing = () => {
-  return (
-    <Header />
-
-  )
-}
+  return <Header />;
+};
 
 export default Landing;
