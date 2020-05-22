9eeddb9ab27eebf922285118f495924d9dc50e9d
Emmanuel Sunday <nuel.mailbox@gmail.com>
2020-05-22 22:41:38 +0100

    Added Footer

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Blog = () => {
   return (
-    <div className='fade-in'>
+    <div className='fade-in Slider-Wrapper'>
       <h1>This is the Blog component!</h1>
       <h1>News and Events content goes here!</h1>
     </div>
