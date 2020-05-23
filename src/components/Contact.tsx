97bd25c40fcb56c7f8eda66f6e248872383ca9d2
Sunday Power Inemesit <sundaypower9@gmail.com>
2020-05-23 23:26:01 +0100

    Fix contact page responsiveness; add anime effects; modify source.

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -19,24 +19,29 @@ import LocationOnIcon from '@material-ui/icons/LocationOn';
 const Contact = () => {
   return (
     <Container fluid className='Contact'>
-      <Container className='title text-center fade-in'>
+      <Row className='map-wrapper'></Row>
+      
+      <Container className='contact-page-title text-center fade-in'>
         <Typography component='h1' variant='h3'>
           CONTACT US
         </Typography>
       </Container>
-      <Container fluid='sm' className='form-container'>
+
+      <Container fluid='sm' className='contact-form-container'>
         <Row className='contact-form-wrapper'>
           <Col lg={9} className='contact-form-col'>
             <Typography
               component='h2'
               variant='h4'
-              className='theme-color-text mb-4 text-center'>
+              className='theme-color-text mb-4 text-center contact-form-header'>
               GET IN TOUCH
             </Typography>
+
             <Typography component='h6' className='font-italic small mb-3'>
               We look forward to getting your feedback or mail about any of our
               projects, products or services.
             </Typography>
+
             <form className='contact-form'>
               <Row>
                 <Col md={6} className='my-2'>
@@ -103,39 +108,44 @@ const Contact = () => {
               </Row>
             </form>
           </Col>
-          <Col lg={3} className='social-media-col'>
+
+          <Col lg={3} className='contact-info-col'>
             <Row className='align-content-center text-center h-100 p-4 py-5'>
+              <Col xs={12} className='p-0'>
+                <Typography
+                  component='h5'
+                  variant='h5'
+                  className='font-weight-bold text-center my-4'>
+                  CONTACT INFORMATION
+                </Typography>
+              </Col>
               <Col xs={12}>
-                <Row>
-                  <Col xs={12} className='p-0'>
-                    <Typography
-                      component='h5'
-                      variant='h5'
-                      className='font-weight-bold text-center my-3'>
-                      CONTACT INFORMATION
-                    </Typography>
+                <Row className='flex-column justify-items-center mb-4'>
+                  <Col className='my-2'>
+                    <LocationOnIcon fontSize='large' />
                   </Col>
-                  <Col xs={12}>
-                    <Row className='flex-column justify-items-center mb-3'>
-                      <Col className='my-2'><LocationOnIcon fontSize='large' /></Col>
-                      <Col className='p-0'>Commercial Plot 6, Golf Course New Market - 9th Mile Expressway, Enugu North, Enugu Nigeria.</Col> 
-                    </Row>
-                    <Row className='flex-column justify-items-center mb-3'>
-                      <Col className='my-2'><EmailIcon fontSize='large' /></Col>
-                      <Col className='p-0'>vee-tek@vee-tek-group.com</Col> 
-                    </Row>
-                    <Row className='flex-column justify-items-center mb-3'>
-                      <Col className='my-2'><PhoneIcon fontSize='large' /></Col>
-                      <Col className='p-0 mb-4'>+2341234567890</Col>
-                    </Row>
+                  <Col className='p-0'>
+                    Commercial Plot 6, Golf Course New Market - 9th Mile
+                    Expressway, Enugu North, Enugu Nigeria.
                   </Col>
+                </Row>
+                <Row className='flex-column justify-items-center mb-4'>
+                  <Col className='my-2'>
+                    <EmailIcon fontSize='large' />
+                  </Col>
+                  <Col className='p-0'>vee-tek@vee-tek-group.com</Col>
+                </Row>
+                <Row className='flex-column justify-items-center'>
+                  <Col className='my-2'>
+                    <PhoneIcon fontSize='large' />
+                  </Col>
+                  <Col className='p-0 mb-4'>+2341234567890</Col>
                 </Row>
               </Col>
             </Row>
           </Col>
         </Row>
       </Container>
-      <Row className='map-wrapper'></Row>
     </Container>
   );
 };
