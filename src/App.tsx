f611e2056b189e5d5dac411be45ecb5d5fbd1019
Sunday Power Inemesit <sundaypower9@gmail.com>
2020-06-06 11:48:08 +0100

    Modify navigation bar and redirection for different companies.

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import {
-  Header,
+  Nav,
   Landing,
   Companies,
-  Blog,
+  Events,
   Gallery,
   Contact,
   About,
@@ -16,11 +16,11 @@ const App = () => {
   return (
     <div className='App fade-in'>
       <BrowserRouter>
-        <Header />
+        <Nav />
         <Switch>
           <Route path={['/', '/index', '/home']} exact component={Landing} />
           <Route path='/companies' component={Companies} />
-          <Route path='/blog' component={Blog} />
+          <Route path='/events' component={Events} />
           <Route path='/gallery' component={Gallery} />
           <Route path='/contact' component={Contact} />
           <Route path='/about' component={About} />
--- src/components/GearsCompany.tsx
+++ src/components/GearsCompany.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const GearsCompany = () => {
   return (
-    <div className='fade-in'>
+    <div className='fade-in my-5 py-5'>
       <h1>This is the GearsCompany component!</h1>
       <h1>GearsCompany content goes here!</h1>
     </div>
