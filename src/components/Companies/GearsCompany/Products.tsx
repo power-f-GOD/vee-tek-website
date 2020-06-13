7003f11b6379be7702907dcea14cec304578db89
Emmanuel Sunday <nuel.mailbox@gmail.com>
2020-06-13 11:02:38 +0100

    Modified Products

--- src/components/Companies/GearsCompany/Products.tsx
+++ src/components/Companies/GearsCompany/Products.tsx
@@ -1,18 +1,23 @@
 import React from "react";
 import { Gear } from "../../../constants";
 import ProductTile from "../../crumbs/ProductTile";
+import { Row, Container } from "react-bootstrap";
 
 const Products = () => {
   return (
+    <Container>
     <div className="Main-Wrapper">
-      {Gear.products.map((item, _) => (
-        <ProductTile 
-          image={item.key}
-          product={item.product}
-          desc={item.desc}
-        />
-      ))}
+      <Row>
+        {Gear.products.map((item, _) => (
+          <ProductTile
+            image={item.key}
+            product={item.product}
+            desc={item.desc}
+          />
+        ))}
+      </Row>
     </div>
+    </Container>
   );
 };
 
--- src/components/crumbs/ProductTile.tsx
+++ src/components/crumbs/ProductTile.tsx
@@ -1,21 +1,38 @@
 import React from "react";
-import { Col, Row, Container } from "react-bootstrap";
+import { Col } from "react-bootstrap";
 import "../../styles/index.min.css";
 
-const ProductTile = (props: { image: string; product: string; desc: string }) => (
-  <div className={"Product-Wrapper-" + props.image + "  align-middle"}>
-    <Container className="p-5 text-white">
-      <Row>
-        <Col sm={12} md={4}>
-          <div className={"Product-Image-" + props.image}></div>
-        </Col>
-        <Col sm={12} md={8}>
-          <h2 className="font-weight-bold">{props.product}</h2>
-          <p className="pt-3">{props.desc}</p>
-        </Col>
-      </Row>
-    </Container>
-  </div>
+// const Stuff = (props: { image: string; product: string; desc: string }) => (
+//   <div className={"Product-Wrapper-" + props.image + "  align-middle"}>
+//     <Container className="p-5 text-white">
+//       <Row>
+//         <Col sm={12} md={4}>
+//           <div className={"Product-Image-" + props.image}></div>
+//         </Col>
+//         <Col sm={12} md={8}>
+//           <h2 className="font-weight-bold">{props.product}</h2>
+//           <p className="pt-3">{props.desc}</p>
+//         </Col>
+//       </Row>
+//     </Container>
+//   </div>
+// );
+
+const ProductCard = (props: {
+  image: string;
+  product: string;
+  desc: string;
+}) => (
+  <Col sm={12} md={4} lg={3} className="mb-4">
+    <div className="Product-Card ">
+      <div className={"Product-Image-" + props.image}> </div>
+      <div className="p-4">
+        <h5>
+          {props.product}
+        </h5>
+      </div>
+    </div>
+  </Col>
 );
 
-export default ProductTile;
+export default ProductCard;
