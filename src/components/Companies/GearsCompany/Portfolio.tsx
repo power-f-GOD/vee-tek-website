cef2dbce095bd477f90d59bab9f2c94ba72ecc9a
Sunday Power Inemesit <sundaypower9@gmail.com>
2020-06-14 14:41:49 +0100

    Add no-image placeholder for clients without image/logo; fix 'portfolio page works' rendering bug on tab switch.

--- src/components/Companies/GearsCompany/Portfolio.tsx
+++ src/components/Companies/GearsCompany/Portfolio.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useRef, useEffect } from 'react';
 
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
@@ -42,24 +42,70 @@ const Portfolio = () => {
         <br />
         <hr className='separator' />
         <Row as='main' className='mx-0 mb-5'>
-          {portfolioData.map((data, key: number) => <Work data={data} key={key} />)}
+          {portfolioData.map((data, key: number) => (
+            <Work data={data} key={key} />
+          ))}
         </Row>
       </Container>
     </Container>
   );
 };
 
-//to do: Add onerror style for images
-
 function Work(props: any) {
+  const _work = useRef() as any;
+  const work = _work.current;
   const { data } = props;
   const { header, description, imgUrl }: Data = data;
   const imgAlt = header + ' logo';
 
+  const hideImageElementOnError = useCallback(
+    (e: any) => {
+      const img = e.target;
+      const noImg = e.target.nextElementSibling;
+
+      img.style.display = 'none';
+      noImg.textContent = header[0].toUpperCase();
+      noImg.style.display = 'inline-flex';
+    },
+    [header]
+  );
+
+  useEffect(() => {
+    const windowHeight = window.innerHeight;
+    const threshold = windowHeight * 0.8;
+
+    if (work) {
+      const animateChildren = () => {
+        const { top } = work.getBoundingClientRect();
+
+        if (top < windowHeight + 200) {
+          if (top < threshold) {
+            work.classList.add('animate-children');
+          } else if (top >= threshold) {
+            work.classList.remove('animate-children');
+          }
+        }
+      };
+
+      document.body.addEventListener('scroll', animateChildren);
+
+      // call function initiallly in case element passes threshold on page load
+      animateChildren();
+    }
+  }, [work]);
+
   return (
-    <Row as='section' className='work mx-0 my-3 debugger w-100 align-content-center'>
+    <section
+      ref={_work}
+      className='work d-flex mx-0 my-3 debugger w-100 align-content-center'>
       <Col md={6} className='work-image-wrapper p-5'>
-        <img src={`/images/portfolio/${imgUrl}`} alt={imgAlt} className='work-image d-inline' />
+        <img
+          src={`/images/portfolio/${imgUrl}`}
+          alt={imgAlt}
+          className='work-image'
+          onError={hideImageElementOnError}
+        />
+        <Col as='span' className='no-image-placeholder work-image' />
       </Col>
       <Col md={6} className='work-desc-container align-self-center h-100 p-0'>
         <Col className='work-desc-wrapper p-5'>
@@ -69,15 +115,11 @@ function Work(props: any) {
             className='work-desc-header mb-2'>
             {header}
           </Typography>
-          <Col className='work-desc p-0'>
-            {description}
-          </Col>
+          <Col className='work-desc p-0'>{description}</Col>
         </Col>
       </Col>
-    </Row>
+    </section>
   );
 }
-
-
 
 export default Portfolio;
--- src/index.tsx
+++ src/index.tsx
@@ -24,24 +24,3 @@ ReactDOM.render(
   </StylesProvider>,
   document.querySelector('#root') || document.createElement('div')
 );
-
-
-const works = document.querySelectorAll('.work');
-const windowHeight = window.innerHeight;
-const threshold = windowHeight * 0.85;
-
-works.forEach((work, index) => {
-  document.body.addEventListener('scroll', () => {
-    const { top } = work.getBoundingClientRect();
-    
-    if (top < windowHeight + 100) {
-      if (top < threshold) {
-      // console.log(index, 'added', top, 'screen', window.innerHeight);
-        work.classList.add('animate-children');
-      } else if (top >= threshold) {
-        // console.log(index, 'removed', top, 'screen', window.innerHeight);
-        work.classList.remove('animate-children');
-      }
-    }
-  });
-});
