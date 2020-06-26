8f2e80642884ea78da4e215e3cff61ad6cce0901
Sunday Power Inemesit <sundaypower9@gmail.com>
2020-06-26 21:14:23 +0100

    Add 'Go up' Floating Action Button (FAB) for to scrollapp to top.

--- src/components/Companies/GearsCompany/Portfolio.tsx
+++ src/components/Companies/GearsCompany/Portfolio.tsx
@@ -16,6 +16,7 @@ import {
   numOfSwitchgearWorks,
   numOfEngineeringWorks
 } from './portfolioDataV2';
+import GoUpFAB from '../../crumbs/GoUpFAB';
 
 export interface Data {
   header: string;
@@ -72,6 +73,18 @@ const Portfolio = () => {
       }
     }
   }, [activeProject, numOfSwitchgearWorksToShow, numOfEngineeringWorksToShow]);
+
+  // const handleShowLessClick = useCallback(() => {
+  //   if (activeProject === 'switchgear') {
+  //     if (numOfSwitchgearWorksToShow > limit) {
+  //       setNumOfSwitchgearWorksToShow((prevNum) => prevNum - limit);
+  //     }
+  //   } else {
+  //     if (numOfEngineeringWorksToShow > limit) {
+  //       setNumOfEngineeringWorksToShow((prevNum) => prevNum - limit);
+  //     }
+  //   }
+  // }, [activeProject, numOfSwitchgearWorksToShow, numOfEngineeringWorksToShow]);
 
   useEffect(() => {
     if (tabIndicator) {
@@ -197,6 +210,18 @@ const Portfolio = () => {
                 textAlign='center'
                 className='d-inline-block w-auto'
                 marginTop='4rem'>
+                {/* {((activeProject === 'switchgear' &&
+                numOfSwitchgearWorksToShow > limit) ||
+                (activeProject === 'engineering' &&
+                  numOfEngineeringWorksToShow > limit)) && (
+                <Button
+                  className='load-more-button outlined my-3 mx-2'
+                  variant='contained'
+                  color='primary'
+                  onClick={handleShowLessClick}>
+                  Show Less <ExpandLessIcon className='ml-2' />
+                </Button>
+              )} */}
                 {((activeProject === 'switchgear' &&
                   numOfSwitchgearWorksToShow < numOfSwitchgearWorks) ||
                   (activeProject === 'engineering' &&
@@ -230,8 +255,8 @@ const Portfolio = () => {
           </Row>
         </Container>
       </Container>
-
       
+      <GoUpFAB />
     </>
   );
 };
