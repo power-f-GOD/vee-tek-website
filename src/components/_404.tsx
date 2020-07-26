c851ff5b5aecada0badcc20704d878b9e71f75b8
Sunday Power Inemesit <sundaypower9@gmail.com>
2020-07-26 21:30:53 +0100

    Complet Pipes Landing page, add all the necessary stuff; Change font for texts, keep initial for headings.

--- src/components/_404.tsx
+++ src/components/_404.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const _404 = () => {
-  React.useEffect(() => () => {
+  React.useEffect(() => {
     window.scrollTo(0, 0);
-  });
-  
+  }, []);
+
   return (
     <div className='fade-in my-5 py-5 container'>
       <h1>404 Page not found!</h1>
