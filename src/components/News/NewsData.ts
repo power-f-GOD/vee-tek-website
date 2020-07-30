e7068fcbaf262a5928d51e6652e46687dc256ed6
Emmanuel Sunday <nuel.mailbox@gmail.com>
2020-07-30 15:52:27 +0100

    worked on news

---
+++ src/components/News/NewsData.ts
@@ -0,0 +1,45 @@
+/**
+ * Welcome to News Control Center 😎
+ * Alright to avoid stories, use the following format to add posts.
+ * 
+ * {
+    title: "Pipes and Fittings",
+    author: "Immaculata Abba",
+    date: "February 2, 2020",
+    paragraph: `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Reprehenderit beatae accusamus totam molestias perspiciatis officiis, ipsum 
+        laudantium, consectetur ipsam ipsa maiores vero aspernatur qui quas est saepe, 
+        soluta eos dolores.`,
+    link: "https://medium.com/",
+    image: "",
+  },
+ * Add new posts on top of the the list.
+ * Eg:
+ * [
+ *  {
+ *      ... your new postss.
+ *  }
+ *  .
+ *  .
+ *  .
+ * {
+ *      ... some old post.    
+ * }
+ * ]
+ * 
+ * Have any questions? reach out to nuel.mailbox@gmail.com or sundaypower9@gmail.com
+ * 
+ */
+
+const data: {
+  title: string;
+  author: string;
+  date: string;
+  paragraph: string;
+  link: string;
+  image: string;
+}[] = [
+  // Your newest post comes right after this
+];
+
+export default data;
