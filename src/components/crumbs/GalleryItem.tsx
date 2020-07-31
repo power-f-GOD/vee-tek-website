7489ad277ac7d6ee1b4c1c389b2f9f0c9050d378
Emmanuel Sunday <nuel.mailbox@gmail.com>
2020-07-31 19:06:43 +0100

    trying to fix gallery

--- src/components/crumbs/GalleryItem.tsx
+++ src/components/crumbs/GalleryItem.tsx
@@ -1,10 +1,27 @@
 import React from "react";
-import { Col, Image } from "react-bootstrap";
+import { Col } from "react-bootstrap";
 
 const GalleryItem = (props: { image: string }) => (
   <Col xs={10} sm={6} md={4} lg={4} className="p-1">
     <div className="Gallery-Image-Wrapper">
-      <Image src={props.image} className="Gallery-Image" />
+      <picture>
+        <source
+          className="Gallery-Image"
+          srcSet={props.image + ".webp"}
+          type="image/webp"
+        />
+        <source
+          className="Gallery-Image"
+          srcSet={props.image + ".jpg"}
+          type="image/jpeg"
+        />
+
+        <img
+          src={props.image + ".jpg"}
+          className="Gallery-Image"
+          alt="gallery item"
+        />
+      </picture>
     </div>
   </Col>
 );
